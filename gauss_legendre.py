26baa098821b21556ea0175a495e92f2ebb03545
Larrynuhuh <swastiksingh1402@gmail.com>
2026-05-08 18:00:31 +0530

    added symmetry, will try to test if it works for various test cases

--- gauss_legendre.py
+++ gauss_legendre.py
@@ -33,7 +33,8 @@ def big_besselj1(x):
     return big_approx
 
 def macmahon(n):
-    k = jnp.arange(11, n+1)
+    m = jax.lax.cond(n % 2 == 0, lambda _: n//2, lambda _: n//2 + 1, operand=None)
+    k = jnp.arange(11, m+1)
     beta = (k - 0.25) * jnp.pi
     expression = beta + (1/(8*beta)) - (31/(384*beta**3)) + (3779/(15360*beta**5))
 
@@ -44,40 +45,44 @@ def j0smallroots():
     18.071063967910922, 21.211636629879258, 24.352471530749302, 27.493479132040254, 30.634606468431975], dtype=jnp.float64)
 
 def interior_nodes(n):
-    k = jnp.arange(1, n+1)
-    phi_k = ((k-0.25) * jnp.pi) / (n + 0.5)
+    m = jax.lax.cond(n % 2 == 0, lambda _: n//2, lambda _: n//2 + 1, operand=None)
+    k = jnp.arange(1, m+1)
+    phi_k = ((k-0.25) * jnp.pi) / (m + 0.5)
 
-    interior = (1 - ((n-1)/(8*n**3)) - 1/(384*n**4) * (39 - (28/(jnp.sin(phi_k)**2)))) * jnp.cos(phi_k)
+    interior = (1 - ((m-1)/(8*m**3)) - 1/(384*m**4) * (39 - (28/(jnp.sin(phi_k)**2)))) * jnp.cos(phi_k)
 
     return interior
 
 def boundary_nodes(n):
-    k = jnp.arange(1, n+1)
+    m = jax.lax.cond(n % 2 == 0, lambda _: n//2, lambda _: n//2 + 1, operand=None)
+    k = jnp.arange(1, m+1)
 
-    j_k = jnp.concatenate((j0smallroots(), macmahon(n)))
+    j_k = jnp.concatenate((j0smallroots(), macmahon(m)))
 
-    psi_k = j_k/(n + 0.5)
+    psi_k = j_k/(m + 0.5)
 
-    boundary_term = psi_k + ((psi_k * 1/jnp.tan(psi_k)) - 1) / (8 * psi_k * (n + 0.5)**2) 
+    boundary_term = psi_k + ((psi_k * 1/jnp.tan(psi_k)) - 1) / (8 * psi_k * (m + 0.5)**2) 
     boundary = jnp.cos(boundary_term)
 
     return boundary
 
 
 def compute_nodes(n):
-    k = jnp.arange(1, n+1)
+    m = jax.lax.cond(n % 2 == 0, lambda _: n//2, lambda _: n//2 + 1, operand=None)
+    k = jnp.arange(1, m+1)
     #k_hat = n - k + 1
 
-    interior = interior_nodes(n)
-    boundary = boundary_nodes(n)
+    interior = interior_nodes(m)
+    boundary = boundary_nodes(m)
 
-    cond = (n - k + 1) <= jnp.ceil(n**(2/3)/jnp.pi).astype(int)
+    cond = (m - k + 1) <= jnp.ceil(m**(2/3)/jnp.pi).astype(int)
 
-    nodes = jnp.where(cond, boundary, interior)
+    nodes_raw = jnp.where(cond, boundary, interior)
+    zero_or_not = jax.lax.cond(n % 2 == 0, lambda _: -nodes_raw[1::-1], lambda _: -nodes_raw[::-1], operand=None)
+    nodes = jnp.concatenate((zero_or_not, nodes_raw))
 
     return nodes  
 
 
-def interior_evaluation():
-    ...
+
 
